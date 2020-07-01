534b38be1db55cd343a328c1b35c76e13a27769a
elysaia <jdiqiao@gmail.com>
2020-07-01 16:33:48 -0400

    added todo list files

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { uuid } from 'uuidv4';
+
+function TodoForm() {
+
+  const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false
+  });
+
+  function handleTaskInputChange(e) {
+    setTodo({ ...todo, task:e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id:uuid.v4() });
+      // reset task input
+      setTodo({ ...todo, task: "" });
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        name="task"
+        type="text"
+        value={todo.task}
+        onChange={handleTaskInputChange}
+      />
+      <button type="submit"/>
+    </form>
+  );
+}
+
+export default TodoForm;
